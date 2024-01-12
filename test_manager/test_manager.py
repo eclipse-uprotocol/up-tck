718a4babdfaf4f8e2559b6d289f6157e4062ae39
SZQPX1_gme <patrick.yang@gm.com>
2024-01-12 15:28:46 -0500

    removed unnecessary files

---
+++ test_manager/test_manager.py
@@ -0,0 +1,19 @@
+
+class TestManager:
+    def __init__(self) -> None:
+        """
+        create the json then serialize possibly? for all the diff methods 
+        """
+        pass
+
+    def send(self):
+        pass
+    def register_listener(self):
+        pass
+    def on_receive(self):
+        pass
+    def unregister_listener(self):
+        pass
+    def ustatus(self):
+        pass
+
