426b8de51e8afa1e8b47dd90b153c08f2c8d8e32
SZQPX1_gme <patrick.yang@gm.com>
2024-02-08 16:16:00 -0500

    added context free grammar for argument parsing

--- python/demo_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python dispatcher/dispatcher.py &
-sleep 1
-python examples/tck_interoperability/test_socket_tm.py &
-sleep 1
-python examples/tck_interoperability/test_socket_ta.py &
