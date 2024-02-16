0f950d5e3ea695df783ce8b0907669129783fd4a
gzm6xl_gme <matthew.dalonzo@gm.com>
2024-02-16 13:28:01 -0500

    [Matthew D'Alonzo] fixed .sh file

--- BDD/testrunner.sh
+++ BDD/testrunner.sh
@@ -1,7 +1,7 @@
 #python -m behave -i updatedoor_method_latest_4vehicle.feature -f behave_html_formatter:HTMLFormatter -o reports/updatedoor_method_latest_4vehicle.html --junit
 echo Enter the feature file name
 read fname
-python -m behave -i testing.feature --format html --outfile reports/"${fname}_$(date +%Y%m%d_%H%M%S).html"
+python -m behave -i "${fname}" --format html --outfile reports/"${fname}_$(date +%Y%m%d_%H%M%S).html"
 
 #behave --no-capture
 #behave
