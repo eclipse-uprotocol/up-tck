ff33af42c76bab3796347777ec1dcc08e456abfd
SZQPX1_gme <patrick.yang@gm.com>
2024-02-20 12:51:37 -0500

    [Matthew D'Alonzo] so much more cleanup

--- BDD/features/steps/tck_step_implementations.py
+++ BDD/features/steps/tck_step_implementations.py
@@ -1,13 +1,33 @@
+# -------------------------------------------------------------------------
+#
+# Copyright (c) 2024 General Motors GTO LLC
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# SPDX-FileType: SOURCE
+# SPDX-FileCopyrightText: 2024 General Motors GTO LLC
+# SPDX-License-Identifier: Apache-2.0
+#
+# -------------------------------------------------------------------------
+
 from behave import when, then, given, step
 from behave.runner import Context
-import sys
 from hamcrest import assert_that, equal_to
 
-# sys.path.append('../../../python/test_manager')
-# from test_manager import testmanager
-
-# sys.path.append('../../../python/up_client_socket_python')
-# from up_client_socket_python import transport_layer as tl
 from uprotocol.transport.ulistener import UListener
 from uprotocol.proto.uattributes_pb2 import UAttributes
 from uprotocol.proto.uri_pb2 import UUri
@@ -15,7 +35,6 @@ from uprotocol.proto.ustatus_pb2 import UStatus, UCode
 from uprotocol.proto.upayload_pb2 import UPayload
 
 received_payload = ''
-
 
 class SocketUListener(UListener):
     def __init__(self, sdk_name: str = "python") -> None:
