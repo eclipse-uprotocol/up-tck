fbf55b85d9e9f6a5b5e832d761844ed0ab5881a5
Matthew D'Alonzo <matthew.dalonzo@gm.com>
2024-03-07 11:31:02 -0500

    Modified testrunner for linux to use python 3

--- BDD/testrunner.sh
+++ BDD/testrunner.sh
@@ -1,3 +1,3 @@
 echo Enter the feature file name
 read fname
-python -m behave -i "${fname}" --format html --outfile reports/"${fname}_$(date +%Y%m%d_%H%M%S).html"
+python3 -m behave -i "${fname}" --format html --outfile reports/"${fname}_$(date +%Y%m%d_%H%M%S).html"
