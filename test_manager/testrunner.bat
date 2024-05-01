5930711b7d3cd6a6f70e303f6de8a20b0651d24d
Matthew D'Alonzo <matthew.dalonzo@gm.com>
2024-05-01 13:29:34 -0400

    Generalize Executables

--- test_manager/testrunner.bat
+++ test_manager/testrunner.bat
@@ -2,6 +2,7 @@
 
 set /p fname=Enter the feature file name:
   set /p language=Enter Language Under Test [python/java]:
+    set /p transport=Enter Transport Under Test [socket]:
 
     REM Run Behave with HTML formatter
-    python -m behave --define uE1=%language% -i %fname% --format html --outfile reports/%fname%_%date:~10,4%%date:~4,2%%date:~7,2%_%RANDOM%.html
+    python -m behave --define uE1=%language% --define transport=%transport% -i %fname% --format html --outfile reports/%fname%_%date:~10,4%%date:~4,2%%date:~7,2%_%RANDOM%.html
--- test_manager/testrunner.sh
+++ test_manager/testrunner.sh
@@ -2,4 +2,5 @@ echo Enter the feature file name
 read fname
 echo Enter Language Under Test [python/java]
 read language
-python3 -m behave --define uE1=%language% -i "${fname}" --format html --outfile reports/"${fname}_$(date +%Y%m%d_%H%M%S).html"
+echo Enter Transport Under Test [socket]
+python3 -m behave --define uE1=%language% --define transport=%socket% -i "${fname}" --format html --outfile reports/"${fname}_$(date +%Y%m%d_%H%M%S).html"
