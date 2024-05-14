65aa8891bede17e801116d065dd37e98b86cb0e1
Patrick Yang <yangshilongpatrick@gmail.com>
2024-05-14 10:12:07 -0400

    testrunner.bat has lang2 option, removed test.py, added spacing

--- test_manager/testrunner.bat
+++ test_manager/testrunner.bat
@@ -1,8 +1,9 @@
 @echo off
 
 set /p fname=Enter the feature file name:
-  set /p language=Enter Language Under Test [python/java]:
+  set /p language1=Enter Language1 Under Test [python/java]:
+  set /p language2=Enter Language2 Under Test [python/java/_blank_]:
     set /p transport=Enter Transport Under Test [socket]:
 
     REM Run Behave with HTML formatter
-    python -m behave --define uE1=%language% --define transport=%transport% -i %fname% --format html --outfile reports/%fname%_%date:~10,4%%date:~4,2%%date:~7,2%_%RANDOM%.html
+    python -m behave --define uE1=%language1% --define uE2=%language2% --define transport=%transport% -i %fname% --format html --outfile reports/%fname%_%date:~10,4%%date:~4,2%%date:~7,2%_%RANDOM%.html
--- test_manager/testrunner2lang.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set /p fname=Enter the feature file name:
-  set /p language1=Enter Language1 Under Test [python/java]:
-  set /p language2=Enter Language2 Under Test [python/java]:
-    set /p transport=Enter Transport Under Test [socket]:
-
-    REM Run Behave with HTML formatter
-    python -m behave --define uE1=%language1% --define uE2=%language2% --define transport=%transport% -i %fname% --format html --outfile reports/%fname%_%date:~10,4%%date:~4,2%%date:~7,2%_%RANDOM%.html
