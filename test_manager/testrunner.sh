abb5ea1efdce56f773d5c2af74fbaab76206e6d5
Matthew D'Alonzo <matthew.dalonzo@gm.com>
2024-07-19 11:33:27 -0400

    Update Python and SocketPython to 1.5.8
    * Update Python Test Agent
    * Update Python Test Manager
    * Update TCK Step Implementations
    * Update uuid_validator feature

--- test_manager/testrunner.sh
+++ test_manager/testrunner.sh
@@ -2,8 +2,10 @@ echo Enter the feature file name
 read fname
 echo Enter Language1 Under Test [python/java/rust/cpp]
 read language1
+echo Enter Transport1 Under Test [socket/zenoh/someip]
+read transport1
 echo Enter Language2 Under Test [python/java/rust/cpp/_blank_]
 read language2
-echo Enter Transport Under Test [socket/zenoh]
-read transport
-python3 -m behave --define uE1="${language1}" --define uE2="${language2}" --define transport="${transport}" -i "${fname}" --format html --outfile reports/"${fname}_$(date +%Y%m%d_%H%M%S).html"
+echo Enter Transport2 Under Test [socket/zenoh/someip]
+read transport2
+python3 -m behave --define uE1="${language1}" --define uE2="${language2}" --define transport1="${transport1}" --define transport2="${transport2}" -i "${fname}" --format html --outfile reports/"${fname}_$(date +%Y%m%d_%H%M%S).html"
