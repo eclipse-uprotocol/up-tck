7328eec8d295a2c5fdf5506d068b8f2490a7ed55
David Bruce <david.e.bruce@icloud.com>
2024-07-23 22:09:10 -0700

    wildcards working but validator alignment necessary

--- up_client_socket/cpp/src/test.cpp
+++ up_client_socket/cpp/src/test.cpp
@@ -28,38 +28,58 @@ int main(int argc, char *argv[])
 
     auto transport = make_shared<SocketUTransport>(def_src_uuri);
 
-    uprotocol::v1::UUri src1;
-    src1.set_authority_name("src1");
-    src1.set_ue_id(0x00010001);
-    src1.set_ue_version_major(1);
-    src1.set_resource_id(0x8000);
+    uprotocol::v1::UUri topic;
+    topic.set_authority_name("topic");
+    topic.set_ue_id(0x00010001);
+    topic.set_ue_version_major(1);
+    topic.set_resource_id(0x8000);
+    auto action_exact = [&](const uprotocol::v1::UMessage& msg) { cout << "## action_exact" << endl; };
 
-    uprotocol::v1::UUri src2;
-    src2.set_authority_name("src2");
-    src2.set_ue_id(0x00010001);
-    src2.set_ue_version_major(1);
-    src2.set_resource_id(0x8000);
+    uprotocol::v1::UUri topic_recv_all_topic;
+    topic_recv_all_topic.set_authority_name("*");
+    topic_recv_all_topic.set_ue_id(0x00010001);
+    topic_recv_all_topic.set_ue_version_major(1);
+    topic_recv_all_topic.set_resource_id(0x8000);
+    auto action_all_topic = [&](const uprotocol::v1::UMessage& msg) { cout << "## action_all_topic" << endl; };
 
-    auto action1 = [&](const uprotocol::v1::UMessage& msg) {
-        cout << "############################ action1" << endl;
-        cout << msg.DebugString() << endl;
-    };
 
-    auto action2 = [&](const uprotocol::v1::UMessage& msg) {
-        cout << "############################ action2" << endl;
-        cout << msg.DebugString() << endl;
-    };
+    uprotocol::v1::UUri topic_recv_all_ue_id;
+    topic_recv_all_ue_id.set_authority_name("topic");
+    topic_recv_all_ue_id.set_ue_id(0xffff);
+    topic_recv_all_ue_id.set_ue_version_major(1);
+    topic_recv_all_ue_id.set_resource_id(0x8000);
+    auto action_all_ue_id = [&](const uprotocol::v1::UMessage& msg) { cout << "## action_all_ue_id" << endl; };
 
-    auto lhandle = transport->registerListener(src1, action1); ///, source_filter);
-    auto lhandle2 = transport->registerListener(src1, action2); ///, source_filter);
-    auto lhandle3 = transport->registerListener(src2, action2); ///, source_filter);
+    uprotocol::v1::UUri topic_recv_all_vers_major;
+    topic_recv_all_vers_major.set_authority_name("topic");
+    topic_recv_all_vers_major.set_ue_id(0x00010001);
+    topic_recv_all_vers_major.set_ue_version_major(0xffff);
+    topic_recv_all_vers_major.set_resource_id(0x8000);
+    auto action_all_vers_major = [&](const uprotocol::v1::UMessage& msg) { cout << "## action_all_vers_major" << endl; };
 
-    for (auto i = 0; i < 2; i++) {
+    uprotocol::v1::UUri topic_recv_all_resource_id;
+    topic_recv_all_resource_id.set_authority_name("topic");
+    topic_recv_all_resource_id.set_ue_id(0x00010001);
+    topic_recv_all_resource_id.set_ue_version_major(1);
+    topic_recv_all_resource_id.set_resource_id(0xffff);
+    auto action_all_resource_id = [&](const uprotocol::v1::UMessage& msg) { cout << "## action_all_resource_id" << endl; };
+
+    auto lhandle0 = transport->registerListener(topic, action_exact); ///, source_filter);
+    // auto lhandle1 = transport->registerListener(topic_recv_all_topic, action_all_topic); ///, source_filter);
+    auto lhandle2 = transport->registerListener(topic_recv_all_ue_id, action_all_ue_id); ///, source_filter);
+    auto lhandle3 = transport->registerListener(topic_recv_all_vers_major, action_all_vers_major); ///, source_filter);
+    // auto lhandle4 = transport->registerListener(topic_recv_all_resource_id, action_all_resource_id); ///, source_filter);
+
+    // auto lhandle2 = transport->registerListener(src1, action2); ///, source_filter);
+    // auto lhandle3 = transport->registerListener(src2, action2); ///, source_filter);
+
+    for (auto i = 0; i < 1; i++) {
         {
             uprotocol::v1::UAttributes attr;
             attr.set_type(uprotocol::v1::UMESSAGE_TYPE_PUBLISH);
             *attr.mutable_id() = make_uuid();
-            *attr.mutable_source() = src1;
+            *attr.mutable_source() = topic;
+            // *attr.mutable_sink() = dest;
             attr.set_payload_format(uprotocol::v1::UPAYLOAD_FORMAT_TEXT);
             attr.set_ttl(1000);
 
