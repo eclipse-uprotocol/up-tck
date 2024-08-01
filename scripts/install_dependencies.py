c2d0806fe8904d6c7c39f33c062bb773a7d0317c
agosh01 <msagosh@gmail.com>
2024-08-01 17:49:04 -0400

    Automating cpp build process

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -32,11 +32,36 @@ def run_command(command):
         print(e.stderr)
         sys.exit(1)
 
+def check_conan_version():
+    try:
+        # Check Conan version
+        result = subprocess.run(["conan", "--version"], capture_output=True, text=True, check=True)
+        version_str = result.stdout.strip().split()[-1]
+        version_parts = version_str.split('.')
+        major, minor = int(version_parts[0]), int(version_parts[1])
+
+        # Check if version is 2.3 or greater
+        if major > 2 or (major == 2 and minor >= 3):
+            print(f"Conan version {version_str} is installed.")
+            return True
+        else:
+            print(f"Conan version {version_str} is lower than the required 2.3.")
+            return False
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        print("Conan is not installed.")
+        return False
 
 def main():
     # Define the repository and desired branch or commit
     repo_url = "https://github.com/eclipse-uprotocol/up-python.git"
     branch = "main"
+
+    if not check_conan_version():
+        print("Conan is not installed or version is lower than 2.3.")
+        sys.exit(1)
+
+    # Install dependencies
+    run_command("pip install -r requirements.txt")
 
     # Directory to be removed and cloned
     repo_dir = "up-python"
@@ -61,9 +86,14 @@ def main():
     # Install the package from the repository
     run_command("pip install .")
 
-    # Optionally, handle other dependencies via requirements.txt
     os.chdir(os.pardir)  # Move back to the root of the cloned repository directory structure
-    run_command("pip install -r requirements.txt")
+
+    run_command(
+        "python install_cpp_test_agent.py "
+        "--up-core-api-version 1.6.0 "
+        "--up-cpp-version 1.0.1-rc1 "
+        "--up-client-socket-version 1.0.0-dev"
+    )
 
 
 if __name__ == "__main__":
